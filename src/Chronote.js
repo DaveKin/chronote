1097225241c8ed4cfe3f563650718fee49f6437a
kinsellad <dave.kinsella@thehutgroup.com>
2017-05-10 00:44:14 +0100

    initial structure

--- src/Chronote.js
+++ src/Chronote.js
@@ -6,12 +6,33 @@ class Chronote {
   }
 
   bind() {
-    this.container = document.querySelector('div');    
+    this.writeButton = document.querySelector('.button_write');
+    this.reviewButton = document.querySelector('.button_review');
+    this.writeInput = document.querySelector('.input_write');
+    this.reviewSection = document.querySelector('.section_review');
+
+    this.writeButton.addEventListener('click', () => {
+      this.setMode('write');
+    });
+    this.reviewButton.addEventListener('click', () => {
+      this.setMode('review');
+    });
+  }
+
+  focusInput() {
+    this.writeInput.focus();
+  }
+
+  setMode(mode) {
+    document.body.className = mode;
+    if(mode==='write'){
+      this.focusInput();
+    }
   }
 
   urlLoaded (url) {
     this.url = url;
-    this.container.innerText = `Loaded from: ${this.url}`;
+    this.reviewSection.innerText = this.url;
   }
 
   // get the url for the current tab
--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   var cn = new Chronote();
+  cn.setMode('write');
+  cn.focusInput();
 });
